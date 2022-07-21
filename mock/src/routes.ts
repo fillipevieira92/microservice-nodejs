ed75a5a7367af83bb7220783d6f055a9ac5bf252
fillipevieira92 <fillipevieira92@gmail.com>
2022-07-21 07:04:37 -0300

    Add: Rotas e Index

---
+++ mock/src/routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+router.get('/users', (req, res) => {
+
+})
+
+router.get('/users/:id', (req, res) => {
+
+})
+
+router.post('/users', (req, res) => {
+
+})
+
+router.put('/users/:id', (req, res) => {
+
+})
+
+router.delete('/users/:id', (req, res) => {
+
+})
+
+export default router;
