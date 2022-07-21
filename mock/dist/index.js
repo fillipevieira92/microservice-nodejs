5059d237df5ba2142e5e0552bc5e038fb7607866
fillipevieira92 <fillipevieira92@gmail.com>
2022-07-21 08:00:41 -0300

    Add: Swagger

--- mock/dist/index.js
+++ mock/dist/index.js
@@ -5,7 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_json_1 = __importDefault(require("./swagger.json"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 app.use('/api/v1', routes_1.default);
 app.listen(3000, () => console.log("Server running on port 3000"));
--- mock/src/index.ts
+++ mock/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./routes";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from './swagger.json';
 
 const app = express();
 
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use('/api/v1', router)
 
    
