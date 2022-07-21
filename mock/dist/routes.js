8ffd6c9d5a0dc3ea0417b4d08cb26e4cc21d26e8
fillipevieira92 <fillipevieira92@gmail.com>
2022-07-21 07:19:20 -0300

    Add: Configuração PostgreSQL | Docker | docker-compose

---
+++ mock/dist/routes.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+router.get('/users', (req, res) => {
+});
+router.get('/users/:id', (req, res) => {
+});
+router.post('/users', (req, res) => {
+});
+router.put('/users/:id', (req, res) => {
+});
+router.delete('/users/:id', (req, res) => {
+});
+exports.default = router;
